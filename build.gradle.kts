7cae4dd5493959c67115b1d0e6e09d0f66fefee2
brokenhappy <wout_werkman@hotmail.com>
2026-07-04 10:20:01 +0200

    Project setup by Claude

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    kotlin("jvm") version "2.4.0"
+}
+
+group = "dev.wout"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
+
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.11.0")
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(27)
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_26)
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+    jvmArgs("--enable-preview")
+}
+
+tasks.withType<JavaExec>().configureEach {
+    jvmArgs("--enable-preview")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(26)
+}
